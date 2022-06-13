c3094305fb5d668bbb1c459e8b82bb455a2ae1e4
Andre Fernandes <fpf.andre@gmail.com>
2022-06-13 19:07:28 -0300

    cria botãozinho pra rotações

--- SurRender/menus/modify_object.py
+++ SurRender/menus/modify_object.py
@@ -109,9 +109,13 @@ class RotateWidget(QWidget):
         self.object_list = object_list
 
         self.combobox = QComboBox()
-        self.angle_line = QLineEdit()
-        self.x_line = QLineEdit()
-        self.y_line = QLineEdit()
+
+        self.x_angle_line = QLineEdit()
+        self.y_angle_line = QLineEdit()
+        self.z_angle_line = QLineEdit()
+
+        self.around_x_line = QLineEdit()
+        self.around_y_line = QLineEdit()
         self.apply_button = QPushButton('Apply')
         self.apply_button.pressed.connect(self.apply_callback)
 
@@ -120,28 +124,34 @@ class RotateWidget(QWidget):
         self.combobox.addItem('Rotate Around Vector')
         self.combobox.activated.connect(self.box_callback)
         
-        self.x_line.setReadOnly(True)
-        self.y_line.setReadOnly(True)
-        self.x_line.setPlaceholderText('Not avaliable')
-        self.y_line.setPlaceholderText('Not avaliable')
+        self.around_x_line.setReadOnly(True)
+        self.around_y_line.setReadOnly(True)
+        self.around_x_line.setPlaceholderText('Not avaliable')
+        self.around_y_line.setPlaceholderText('Not avaliable')
 
         layout = QFormLayout()
         layout.addRow('', self.combobox)
-        layout.addRow('Angle', self.angle_line)
-        layout.addRow('X', self.x_line)
-        layout.addRow('Y', self.y_line)
+        layout.addRow('Angle x', self.x_angle_line)
+        layout.addRow('Angle y', self.y_angle_line)
+        layout.addRow('Angle z', self.z_angle_line)
+        layout.addRow('Around X', self.around_x_line)
+        layout.addRow('Around Y', self.around_y_line)
         layout.addRow('', self.apply_button)
         self.setLayout(layout)
 
     def apply_callback(self):
         try:
-            x = self.x_line.text()
-            y = self.y_line.text()
-            a = self.angle_line.text()
-            x = float(x) if x else 1
-            y = float(y) if y else 1
-            a = float(a) if a else 0
-            a = np.radians(a)
+            around_x = self.around_x_line.text()
+            around_y = self.around_y_line.text()
+            angle_x = self.x_angle_line.text()
+            angle_y = self.y_angle_line.text()
+            angle_z = self.z_angle_line.text()
+
+            around_x = float(around_x) if around_x else 1
+            around_y = float(around_y) if around_y else 1
+            angle_x = np.radians(float(angle_x) if angle_x else 0)
+            angle_y = np.radians(float(angle_y) if angle_y else 0)
+            angle_z = np.radians(float(angle_z) if angle_z else 0)
 
             i = self.object_list.selected_index()
             shape = self.viewport.scene.shapes[i]
@@ -152,9 +162,12 @@ class RotateWidget(QWidget):
             elif txt == 'Rotate Around Center':
                 around = shape.center()
             elif txt == 'Rotate Around Vector':
-                around = Vector(x,y)
+                around = Vector(around_x, around_y)
 
-            shape.rotate(a, around)
+            shape.rotate_x(angle_x, around)
+            shape.rotate_y(angle_y, around)
+            shape.rotate_z(angle_z, around)
+
             self.viewport.update()
 
         except IndexError:
@@ -163,12 +176,13 @@ class RotateWidget(QWidget):
     
     def box_callback(self):
         ro = self.combobox.currentText() != 'Rotate Around Vector'
-        self.x_line.setReadOnly(ro)
-        self.y_line.setReadOnly(ro)
+
+        self.around_x_line.setReadOnly(ro)
+        self.around_y_line.setReadOnly(ro)
 
         if ro:
-            self.x_line.setPlaceholderText('Not avaliable')
-            self.y_line.setPlaceholderText('Not avaliable')
+            self.around_x_line.setPlaceholderText('Not avaliable')
+            self.around_y_line.setPlaceholderText('Not avaliable')
         else:
-            self.x_line.setPlaceholderText('')
-            self.y_line.setPlaceholderText('')
+            self.around_x_line.setPlaceholderText('')
+            self.around_y_line.setPlaceholderText('')
