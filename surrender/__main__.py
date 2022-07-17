63875d64692a0ded20f8c702953e67af0f72db06
Andre Fernandes <fpf.andre@gmail.com>
2022-07-17 00:11:26 -0300

    Adiciona modo noturno

--- surrender/__main__.py
+++ surrender/__main__.py
@@ -1,11 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+import surrender.breeze_resources  # noqa: F401
 from surrender.main_window import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QFile, QTextStream
 
 
 def main():
     app = QApplication(sys.argv)
-    # app.setStyleSheet(dark_stylesheet)
+
+    file = QFile(":/dark/stylesheet.qss")
+    file.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(file)
+    app.setStyleSheet(stream.readAll())
+
     MainWindow()
     sys.exit(app.exec_())
 
