6aa015edce742723104230a6e42ebceb8915838a
Andre Fernandes <fpf.andre@gmail.com>
2022-07-16 22:01:16 -0300

    Adiciona arquivos para usar dark mode

--- surrender/__main__.py
+++ surrender/__main__.py
@@ -1,11 +1,13 @@
 import sys
-
 from PyQt5.QtWidgets import QApplication
-
 from surrender.main_window import MainWindow
 
-app = QApplication(sys.argv)
 
-frame = MainWindow()
+def main():
+    app = QApplication(sys.argv)
+    # app.setStyleSheet(dark_stylesheet)
+    MainWindow()
+    sys.exit(app.exec_())
 
-sys.exit(app.exec_())
+
+main()
