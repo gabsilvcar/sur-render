05867d7b7b132603654bde1224abe5acc834f62f
André Fernandes <fpf.andre@gmail.com>
2022-08-08 12:18:25 -0300

    Corrige problemas causados pelo isort

--- surrender/shapes/__init__.py
+++ surrender/shapes/__init__.py
@@ -1,10 +1,14 @@
-from .bezier import Bezier
-from .bicubic_bezier import BicubicBezier
-from .bicubic_bspline import BicubicBspline
-from .bspline import BSpline
-from .cube import Cube
-from .line import Line
-from .object_3d import Object3D
+# isort: off
+
 from .point import Point
+from .line import Line
 from .polygon import Polygon
 from .rectangle import Rectangle
+from .bezier import Bezier
+from .bspline import BSpline
+from .cube import Cube
+from .object_3d import Object3D
+from .bicubic_bezier import BicubicBezier
+from .bicubic_bspline import BicubicBspline
+
+# isort: on
